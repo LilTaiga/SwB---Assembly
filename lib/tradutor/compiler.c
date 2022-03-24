f2881bfa57523dd762be48f78ed99ecf70466c14
Matheus de Azevedo Mar Pereira DE <>
2022-03-24 15:25:56 +0000

    Tirei as partes desnecessárias. Atualizei alguns erros antigos

--- lib/tradutor/compiler.c
+++ lib/tradutor/compiler.c
@@ -19,28 +19,6 @@ typedef struct stack_info
 //temos no maximo 5 variaveis locais.
 stack_info stack[5];
 
-// Aqui é onde o processo de compilação de BPL para Assembly ocorre.
-// Todas as funções relacionadas a gerar código Assembly são feitas aqui.
-
-// Essa função é chamada quando encontra-se uma função em BPL
-// Buffer atual: "function fN {pX1 {pX2 {pX3}}}"
-// Esta função retorna quando encontra-se a palavra-chave "end"
-
-void process_attribution()
-{
-  matches = sscanf(buffer, "vi%d = %ci%d %c %ci%d", &main_variable, &variable_type[0], &variable_number[0], &operation, &variable_type[1], &variable_number[1]);
-		// Caso de atribuicao.
-		if(matches == 3){
-			if(variable_type[0] == 'c') printf("    movl $%d, %d(%%rbp)\n", variable_number[0], v1[main_variable-1].pile_place);
-			if(variable_type[0] == 'v') printf("    movl %d(%%rbp), %d(%%rbp)\n", v1[variable_number[0]-1].pile_place, v1[main_variable-1].pile_place);
-			if(variable_type[0] == 'p'){
-				if(variable_number[0] == 1) printf("    movl %%edi, %d(%%rbp)\n", v1[main_variable-1].pile_place);
-				else if(variable_number[0] == 2) printf("    movl %%esi, %d(%%rbp)\n", v1[main_variable-1].pile_place);
-				else printf("    movl %%edx, %d(%%rbp)\n", v1[main_variable-1].pile_place);
-			}
-		}
-}
-
 void compile_function()
 {
 	// Registradores:
@@ -103,9 +81,9 @@ void process_local_variables()
 		{
 			sscanf(buffer, "var vi%d", &index);
 			
-			required_bytes += 4;
+			required_bytes = 4;
 
-			v1[index-1].pile_place = required_bytes * (-1); 
+			stack[index-1].offset = required_bytes * (-1); 
 			continue;
 		}
 		if(strncmp(buffer, "vet", 3) == 0)
@@ -115,8 +93,8 @@ void process_local_variables()
 
 			required_bytes += 4 * vector_size;
 
-			v2[index-1].size = vector_size;
-			v2[index-1].pile_place = required_bytes * (-1);
+			stack[index-1].size = vector_size;
+			stack[index-1].offset = required_bytes * (-1);
 		}
 
 
@@ -141,19 +119,8 @@ void process_instructions()
 
 	read_line();	// Buffer atual: ??? ("end" ou instrução)
 
-	// Enquanto não encontrarmos o final da função, iremos processar as operações.
-	// Processar as operações é um simples caso de casar a string de formatação.
-	// Ao chegar no final da função, pare.
-
-
-	int matches = 0; //Numero de acertos na funcao strncmp.
-	char operation; // tipo da operaçao.
-	int main_variable; // Numero da vaariavel a ser alterada.
-	char variable_type[2]; // numero das variaveis usadas na operacao.
-	int variable_number[2]; // tipo da variavel utilizada.
 	while(strncmp(buffer, "end", 3) != 0)
-	{ 
-		if(strncmp(buffer, "vi", 2) == 0) process_atribution();
+	{
 		if(strncmp(buffer, "get", 3) == 0) process_vector_getter();
 		//if(strncmp(buffer, "set", 2) == 0) process_vector_setter();
 		read_line();
@@ -173,24 +140,24 @@ void process_vector_getter()
 	int target_index;	// índice do target
 
 	sscanf(buffer, "get %ca%d index ci%d to %ci%d", 
-		&vec_type, &vec_index, vec_offset,
+		&vec_type, &vec_index, &vec_offset,
 		&target_type, &target_index);
 
-	char register_pointer[3];
+	char register_pointer[4];
 
-	if(vec_type == "v")
+	if(vec_type == 'v')
 	{
-		register_pointer = "rbp";
+		strcpy(register_pointer, "rbp");
 		// TODO: Calcular a posição do ponteiro na pilha.
 	}
 	else	// vec_type = "p"
 	{
 		if(vec_index == 1)
-			register_pointer = "rdi";
+			strcpy(register_pointer, "edi");
 		else if(vec_index == 2)
-			register_pointer = "rsi";
+			strcpy(register_pointer, "esi");
 		else	// vec_index = 3
-			register_pointer = "rdx";
+			strcpy(register_pointer, "edx");
 
 		vec_offset *= 4;
 
