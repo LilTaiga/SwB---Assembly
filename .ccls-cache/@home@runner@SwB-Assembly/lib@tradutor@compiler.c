e0ccf4977d416a933014bdaebe72c85b0b1282a9
LilTaiga <57048464+LilTaiga@users.noreply.github.com>
2022-03-24 15:33:34 +0000

    -a

--- .ccls-cache/@home@runner@SwB-Assembly/lib@tradutor@compiler.c
+++ .ccls-cache/@home@runner@SwB-Assembly/lib@tradutor@compiler.c
@@ -7,7 +7,7 @@ int function_initialization();
 void process_local_variables();
 void process_instructions();
 void process_attribuition();
-void process_vector_getter();a
+void process_vector_getter();
 
 //Essas structs servem para salvarmos as informacoes necessarias e uteis das nossas variaveis locais para facilitar o uso do registrador da pilha correspondentes a uma variavel.
 typedef struct stack_info
